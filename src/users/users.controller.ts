7ae2f8fee83f7d55c21671aaf97ef5413f1969bf
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-15 18:06:59 +0300

    feat: implement user authentication and current user logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,13 +9,17 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
@@ -23,9 +27,11 @@ export class UsersController {
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
+
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
   @Post('/signout')
   signOut(@Session() session: any) {
