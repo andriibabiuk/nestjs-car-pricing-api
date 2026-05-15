6b1cf8ecfb7bf8163c66f90761017e8790dcd79c
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-15 20:56:43 +0300

    test: verify whoami endpoint after successful signup

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -66,6 +66,19 @@ describe('Authentication System (e2e)', () => {
 
     expect(res.body.email).toEqual(email);
   });
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdfs@asdaf.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie || [])
+      .expect(200);
+    expect(body.email).toEqual(email);
+  });
 
   afterEach(async () => {
     await app.close();
