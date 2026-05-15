8d2e01aa774a0fe96a48ed81feafd09791c655a1
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-15 16:39:07 +0300

    feat: added serialize interceptor and user.dto for responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,16 +9,20 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
-@Controller('users')
+@Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
     return await this.usersService.create(body.email, body.password);
   }
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
