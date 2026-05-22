7c43d30bfd92d238348a7a63d786e314f57f1088
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-22 18:18:54 +0300

    feat: add estimate creation

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,21 +1,30 @@
 import {
   Body,
   Controller,
-  Post,
-  UseGuards,
-  Patch,
+  Get,
   Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AdminGuard } from 'src/guards/admin.guard';
 import { AuthGuard } from '../guards/auth.guard';
-import { CreateReportDto } from './dtos/create-report.dto';
-import { ReportsService } from './reports.service';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
-import { AdminGuard } from 'src/guards/admin.guard';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimage.dto';
+import { ReportsService } from './reports.service';
+
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
