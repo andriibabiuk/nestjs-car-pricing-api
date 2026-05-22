31ad236f07e5b8f67126628d9f040e45d4f74717
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-22 18:01:43 +0300

    feat: add current-user middleware and admin guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -12,6 +12,7 @@ import { ReportsService } from './reports.service';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
@@ -23,7 +24,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
